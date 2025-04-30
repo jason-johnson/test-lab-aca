cb1b56a8ced71d1b4ff2f0757bba41362a3a214e
Jason Johnson <jasojohnson@microsoft.com>
2025-04-30 16:08:29 +0200

    add initial terraform files

---
+++ infra/provider.tf
@@ -0,0 +1,46 @@
+terraform {
+  backend "azurerm" {}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.21.1"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.1.0"
+    }
+    azapi = {
+      source = "Azure/azapi"
+      version = "2.2.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.7.1"
+    }
+    namep = {
+      source  = "jason-johnson/namep"
+      version = "~> 2.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+provider "namep" {}
+
+data "azuread_client_config" "current" {}
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_subscription" "current" {}
---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "salt" {
+  description = "optional salt for use in the name"
+  default = ""
+}
+
+variable "location" {
+  description = "default location to use if not specified"
+  default = "switzerlandnorth"  
+}
+
+variable "app_name" {
+  description = "Name of the application"
+  default = "testea"
+}
