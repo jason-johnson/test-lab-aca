311f821775a334396005302e7eee75ef94e25219
Jason Johnson <jasojohnson@microsoft.com>
2025-05-22 17:37:13 +0200

    fix: update location for service plan and function app to 'westeurope'

--- infra/function.tf
+++ infra/function.tf
@@ -9,7 +9,7 @@ resource "azurerm_storage_account" "fa" {
 resource "azurerm_service_plan" "fa" {
   name                = provider::namep::namestring("azurerm_app_service_plan", local.namep_config, { name = "fa" })
   resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  location            = "westeurope"
   os_type             = "Linux"
   sku_name            = "B1"
 }
@@ -17,7 +17,7 @@ resource "azurerm_service_plan" "fa" {
 resource "azurerm_linux_function_app" "main" {
   name                = provider::namep::namestring("azurerm_function_app", local.namep_config)
   resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  location            = "westeurope"
 
   storage_account_name       = azurerm_storage_account.fa.name
   storage_account_access_key = azurerm_storage_account.fa.primary_access_key
