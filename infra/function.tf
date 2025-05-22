36d3594b043b7c89b06c537c26c6788e52333024
Jason Johnson <jasojohnson@microsoft.com>
2025-05-22 21:14:56 +0200

    fix: update service plan SKU to P0v3 for Linux function app

--- infra/function.tf
+++ infra/function.tf
@@ -11,7 +11,7 @@ resource "azurerm_service_plan" "fa" {
   resource_group_name = azurerm_resource_group.main.name
   location            = "westeurope"
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "P0v3"
 }
 
 resource "azurerm_linux_function_app" "main" {
