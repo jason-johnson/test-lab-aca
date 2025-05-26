018937f377b8b5064ece3502da613da3b5d6a2fd
Jason Johnson <jasojohnson@microsoft.com>
2025-05-26 18:44:03 +0200

    fix: update logging for message delivery count in ServiceBus trigger

--- app/function_app.py
+++ app/function_app.py
@@ -11,4 +11,4 @@ async def sbMessage(msg: func.ServiceBusMessage) -> None:
     logging.info(f"Python ServiceBus queue trigger function processed message: {msg.get_body().decode()}")
     logging.info(f"Message ID: {msg.message_id}")
     logging.info(f"Message Enqueued Time: {msg.enqueued_time_utc}")
-    logging.info(f"Message Dequeue Count: {msg.dequeue_count}")
+    logging.info(f"Message Delivery Count: {msg.delivery_count}")
