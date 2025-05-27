59069c29b4f8519eda033802858272f81acd7c58
Jason Johnson <jasojohnson@microsoft.com>
2025-05-27 16:21:47 +0200

    fix: enhance logging for ServiceBus messages with JSON handling

--- app/function_app.py
+++ app/function_app.py
@@ -1,3 +1,4 @@
+import json
 import os
 import azure.functions as func
 import logging
@@ -8,7 +9,23 @@ app = func.FunctionApp()
 @app.service_bus_queue_trigger(arg_name="msg", queue_name=os.environ["QUEUE_NAME"],
                                connection=os.environ["QUEUE_CONNECTION"])
 async def sbMessage(msg: func.ServiceBusMessage) -> None:
-    logging.info(f"Python ServiceBus queue trigger function processed message: {msg.get_body().decode()}")
-    logging.info(f"Message ID: {msg.message_id}")
-    logging.info(f"Message Enqueued Time: {msg.enqueued_time_utc}")
-    logging.info(f"Message Delivery Count: {msg.delivery_count}")
+    body = msg.get_body().decode()
+    isJson = False
+
+    try:
+        b = json.loads(body)
+        isJson = True
+        body = b
+    except json.JSONDecodeError:
+        pass
+
+    if isJson:
+        logging.info(f"#### Python ServiceBus queue trigger function processed JSON message: {body}")
+    else:
+        logging.info(f"#### Python ServiceBus queue trigger function processed message: {body}")
+
+    logging.info(f"#### Message ID: {msg.message_id}")
+    logging.info(f"#### Message Enqueued Time: {msg.enqueued_time_utc}")
+    logging.info(f"#### Message Content Type: {msg.content_type}")
+    logging.info(f"#### Message Delivery Count: {msg.delivery_count}")
+    # asyncio.sleep(5)
