93a4892cb8d74cacb1a75df508f48e7bcbe0f058
Jason Johnson <jasojohnson@microsoft.com>
2025-05-28 14:35:18 +0200

    move scale rules to function.tf

--- infra/function.tf
+++ infra/function.tf
@@ -64,3 +64,41 @@ resource "azurerm_role_assignment" "fa_sb" {
   role_definition_name = "Azure Service Bus Data Owner"
   principal_id         = azurerm_linux_function_app.main.identity[0].principal_id
 }
+
+resource "azurerm_monitor_autoscale_setting" "fa" {
+  name                = "faAutoscaleSetting"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  target_resource_id  = azurerm_service_plan.fa.id
+
+  profile {
+    name = "defaultProfile"
+
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 4
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "ActiveMessages"
+        metric_resource_id = azurerm_servicebus_namespace.main.id
+        metric_namespace   = "microsoft.servicebus/namespaces"
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT10M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 1000
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT10M"
+      }
+    }
+  }
+}
