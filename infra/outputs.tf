dcde30aaa576a08d647a5b05feab47afc4c6a430
Jason Johnson <jasojohnson@microsoft.com>
2025-06-02 09:58:16 +0200

    fix: rename output for ACA queue and add output for FA queue

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,6 +2,10 @@ output "fully_qualified_namespace" {
     value = replace(replace(azurerm_servicebus_namespace.main.endpoint, "https://", ""), ":443/", "")  
 }
 
-output "queue_name" {
+output "aca_queue_name" {
     value = azurerm_servicebus_queue.aca.name
+}
+
+output "fa_queue_name" {
+    value = azurerm_servicebus_queue.fa.name
 }
