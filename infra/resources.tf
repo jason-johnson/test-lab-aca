bc86e0344b46632120afbf624bba9a4dedecdf96
Jason Johnson <jasojohnson@microsoft.com>
2025-06-25 17:17:44 +0200

    refactor: remove unused resources from resources.tf

--- infra/resources.tf
+++ infra/resources.tf
@@ -7,22 +7,3 @@ data "azurerm_container_registry" "main" {
   name                = "acrjbjmcap1"
   resource_group_name = "rg-manual"
 }
-
-resource "azurerm_container_app_environment" "main" {
-  name                       = provider::namep::namestring("azurerm_container_app_environment", local.namep_config)
-  location                   = azurerm_resource_group.main.location
-  resource_group_name        = azurerm_resource_group.main.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
-}
-
-resource "azurerm_storage_account" "main" {
-  name                     = provider::namep::namestring("azurerm_storage_account", local.namep_config)
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "time_rotating" "main" {
-  rotation_days = 60
-}
