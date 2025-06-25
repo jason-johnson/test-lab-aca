5e199dbf9089e731c82dfd3af836a7ba034d5ef3
Jason Johnson <jasojohnson@microsoft.com>
2025-06-25 17:09:16 +0200

    add health probe and spam background function

--- app/server.py
+++ app/server.py
@@ -1,4 +1,5 @@
 import asyncio
+import contextlib
 import logging
 import sys
 from aiohttp import web
@@ -11,9 +12,34 @@ async def handle(request):
 
     return web.json_response(result)
 
+def health_probe(req):
+    logging.info('#### Health Probe Triggered')
+    # Always healthy as we don't do any real processing of messages
+    return web.json_response(
+        "Healthy",
+        status=200
+    )
+
+async def do_spam(app: web.Application):
+    logging.info('#### Spammer started')
+    while True:
+        await asyncio.sleep(0.1)  # Simulate some delay between spam actions
+        logging.info('#### Spammer is running')
+
+async def background_tasks(app):
+    app[spammer] = asyncio.create_task(do_spam(app))
+
+    yield
+
+    app[spammer].cancel()
+    with contextlib.suppress(asyncio.CancelledError):
+        await app[spammer]
+
 app = web.Application()
+spammer = web.AppKey("spammer", asyncio.Task[None])
 app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+                web.get('/health_probe', health_probe)])
+app.cleanup_ctx.append(background_tasks)
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stderr, level=logging.INFO)
